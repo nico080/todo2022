f21c2acee2855abcc5477b0cf93eed8dbaec2a2f
nico <ncioagb@gmail.com>
2022-09-21 06:55:35 +0800

    Added loader

--- components/Meta.js
+++ components/Meta.js
@@ -8,6 +8,7 @@ const Meta = ({ title, keywords, description }) => {
       <meta name='description' content={description} />
       <meta charSet='utf-8' />
       <link rel='icon' href='/favicon.ico' />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />  
       <title>{title}</title>
     </Head>
   )
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { DataContext } from "../utilities/DataContext";
 import axios from "axios";
 import Pagination from "@material-ui/lab/Pagination";
 import { useRouter } from "next/router";
-
+import NProgress from 'nprogress';
 
 
 export default function Home({ taskItems }) {
@@ -14,102 +14,118 @@ export default function Home({ taskItems }) {
   const [task, setTask] = useState("");
   const [taskList, setTaskList] = useState(taskItems.tasks);
   const [taskListPages, setTaskListPages] = useState(taskItems.totalPages);
-  const [taskListLimits, setTaskListLimits] = useState(router?.query.limit == null ? 5 :router.query.limit);
+  const [taskListLimits, setTaskListLimits] = useState(
+    router?.query.limit == null ? 5 : router.query.limit
+  );
   const [taskListFilter, setTaskListFilter] = useState(null);
 
-
   const handleSubmit = (event) => {
+    
     if (event.key === "Enter" && event.target.value !== "") {
-
-        let newTask = { task, done: false };
-
-        axios.post(`${server}/api/tasks`, newTask)
+      let newTask = { task, done: false };
+      NProgress.start();
+      axios
+        .post(`${server}/api/tasks`, newTask)
         .then((response) => {
+          NProgress.done(false)
           return response.data.task;
         })
         .then((data) => {
-          taskList.pop();
-          setTaskList((taskList) => [
-            data,
-            ...taskList,
-          ]);
+          if (taskList.length>4) taskList.pop();
+
+          setTaskList((taskList) => [data, ...taskList]);
           setTask("");
         })
         .catch((error) => {
+          NProgress.done(false)
           console.log(error.response.data.error);
         });
-     
     }
   };
 
-  const handleUpdateStatus = (id,status) => {
-   axios.patch(`${server}/api/tasks/item/${id}`, {done: !status})
-   .then((response) => {
-     return response.data;
-   })
-   .then((data) => {   
-    taskList.find(obj=> obj._id === data.data._id).done = data.data.done
-    setTaskList(taskList) 
-  })
-   .catch((error) => {
-     console.log(error.response.data.error);
-   });
+  const handleUpdateStatus = (id, status) => {
+    NProgress.start();
+    axios
+      .patch(`${server}/api/tasks/item/${id}`, { done: !status })
+      .then((response) => {
+        NProgress.done(false)
+        return response.data;
+      })
+      .then((data) => {
+        taskList.find((obj) => obj._id === data.data._id).done = data.data.done;
+        setTaskList(taskList);
+      })
+      .catch((error) => {
+        console.log(error.response.data.error);
+      });
   };
 
   const handleDeleteTask = (id) => {
-    axios.delete(`${server}/api/tasks/item/${id}`)
-    .then((response) => {
-      return response.data;
-    })
-    .then((data) => {
-      setTaskList(taskList=> taskList.filter(task=>task._id !== id)) 
-    })
-    .catch((error) => {
-      console.log(error.response.data.error);
-    });
-   };
+    NProgress.start();
 
+    axios
+      .delete(`${server}/api/tasks/item/${id}`)
+      .then((response) => {
+        NProgress.done(false)
+        return response.data;
+      })
+      .then((data) => {
+        setTaskList((taskList) => taskList.filter((task) => task._id !== id));
+      })
+      .catch((error) => {
+        console.log(error.response.data.error);
+      });
+  };
 
-   const getAllTask = (filter,skips) => {
-    console.log(`${server}/api/tasks?${filter == null ? '' : `done=${filter}` }&limit=${taskListLimits}&skips=${skips == null? 0 : skips}`)
-    axios.get(`${server}/api/tasks?${filter == null ? '' : `done=${filter}` }&limit=${taskListLimits}&skips=${skips == null? 0 : skips}`)
-    .then((response) => {
-      return response.data;
-    })
-    .then((data) => {
-      setTaskList(data.tasks) 
-      setTaskListPages(data.totalPages)
-      setTaskListFilter(filter == null? null :filter)
-    })
-    .catch((error) => {
-      console.log(error.response.data.error);
-    });
-   };
+  const getAllTask = (filter, skips) => {
+    NProgress.start();
 
-   const removeComp = () => {
-    axios.delete(`${server}/api/tasks`)
-    .then((response) => {
-      return response.data;
-    })
-    .then((data) => {
-      setTaskList(data.tasks) 
+    axios
+      .get(
+        `${server}/api/tasks?${
+          filter == null ? "" : `done=${filter}`
+        }&limit=${taskListLimits}&skips=${skips == null ? 0 : skips}`
+      )
+      .then((response) => {
+        NProgress.done(false)
+        return response.data;
+      })
+      .then((data) => {
+        setTaskList(data.tasks);
+        setTaskListPages(data.totalPages);
+        setTaskListFilter(filter == null ? null : filter);
+      })
+      .catch((error) => {
+        console.log(error.response.data.error);
+      });
+  };
 
-    })
-    .catch((error) => {
-      console.log(error.response.data.error);
-    });
-   };
+  const removeComp = () => {
+    NProgress.start();
 
+    axios
+      .delete(`${server}/api/tasks`)
+      .then((response) => {
+        NProgress.done(false)
+        return response.data;
+      })
+      .then((data) => {
+        setTaskList(data.tasks);
+      })
+      .catch((error) => {
+        console.log(error.response.data.error);
+      });
+  };
 
-   const handleChangePage = (e,v) => {
-    getAllTask(taskListFilter == null? null: taskListFilter,v == 1? 0 : (+v - 1) *  taskListLimits )
-   };
-
-
+  const handleChangePage = (e, v) => {
+    getAllTask(
+      taskListFilter == null ? null : taskListFilter,
+      v == 1 ? 0 : (+v - 1) * taskListLimits
+    );
+  };
 
   return (
     <>
-    
       <input
         className={taskStyles.inputTXT}
         onKeyPress={handleSubmit}
@@ -120,35 +136,68 @@ export default function Home({ taskItems }) {
         value={task}
       />
 
-<div className={taskStyles.btnGroup}>
-<button onClick = {()=>{getAllTask()}} className={` ${ taskStyles.button0}`}>All</button>
-<button onClick = {()=>{
-    
-  getAllTask(true,0)
-  
-  }} className={` ${ taskStyles.button1}`}>Done</button>
-<button onClick = {()=>{
-    
-    getAllTask(false,0)
-    
-    }} className={`${ taskStyles.button5}`}>In Progress</button>
-<button onClick = {()=>{removeComp()}} className={` ${ taskStyles.button3}`}>Remove completed</button>
-</div>
+      <div className={taskStyles.btnGroup}>
+        <button
+          onClick={() => {
+            getAllTask();
+          }}
+          className={` ${taskStyles.button0}`}
+        >
+          All
+        </button>
+        <button
+          onClick={() => {
+            getAllTask(true, 0);
+          }}
+          className={` ${taskStyles.button1}`}
+        >
+          Done
+        </button>
+        <button
+          onClick={() => {
+            getAllTask(false, 0);
+          }}
+          className={`${taskStyles.button5}`}
+        >
+          In Progress
+        </button>
+        <button
+          onClick={() => {
+            removeComp();
+          }}
+          className={` ${taskStyles.button3}`}
+        >
+          Remove completed
+        </button>
+      </div>
 
       <div className={taskStyles.grid}>
-        <DataContext.Provider value={[taskList,handleUpdateStatus,handleDeleteTask]}>
-        { taskList.length > 0 ? <TaskList /> : <div className={` ${ taskStyles.noTask}`}>no task found</div>}
-           
+        <DataContext.Provider
+          value={[taskList, handleUpdateStatus, handleDeleteTask]}
+        >
+          {taskList.length > 0 ? (
+            <TaskList />
+          ) : (
+            <div className={` ${taskStyles.noTask}`}>no task found</div>
+          )}
         </DataContext.Provider>
       </div>
 
-      <Pagination onChange={handleChangePage} count={taskListPages} size="large" />
+      <Pagination
+        onChange={handleChangePage}
+        count={taskListPages}
+        size="large"
+      />
     </>
   );
 }
 
 export const getServerSideProps = async (context) => {
-  const res = await axios.get(`${server}/api/tasks?limit=${context.query.limit == null? 5:context.query.limit}`);
+  const res = await axios.get(
+    `${server}/api/tasks?limit=${
+      context.query.limit == null ? 5 : context.query.limit
+    }`
+  );
   const taskItems = res.data;
 
   return {
