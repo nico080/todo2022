a51e23f2866b5aab87a80ec78e3ae2541cc4603c
nico <ncioagb@gmail.com>
2022-10-06 23:10:07 +0800

    added timestamp

--- models/task.js
+++ models/task.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
 	task: { type: String, required: true },
 	done: { type: Boolean, default: false },
-});
+}, { timestamps: { createdAt: 'createdAt' } });
 
 
 export default mongoose.models.task || mongoose.model("task", taskSchema);
